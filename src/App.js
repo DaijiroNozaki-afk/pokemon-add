d1d9746870321c92ebfe0bc9e4065c71045700d1
dnozaki <dnozaki88888888@gmail.com>
2023-03-24 11:01:07 +0900

    implement carComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
@@ -35,7 +36,11 @@ function App() {
         <h1>ロード中・・・</h1>
       ) : (
         <>
-          <h1>ポケモンデータを取得しました</h1>
+        <div className="pokemonCardcontainer">
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon} />
+          })}
+        </div>
         </>
       )}
     </div>
