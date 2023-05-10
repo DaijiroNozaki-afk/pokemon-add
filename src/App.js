ddcbf46678582ce150b1c34ad2b1d53e2d65f04c
dnozaki <dnozaki88888888@gmail.com>
2023-05-10 16:40:01 +0900

    implement pokemonDetailedData

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import { useEffect } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 import { useState } from 'react';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/"
   const [loading, setLoading] = useState(true)
+  const [pokemonData, setPokemonData] = useState([])
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL)
-      console.log(res)
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results)
+      // console.log(res.results)
       setLoading(false)
 
     } 
     fetchPokemonData()
   }, [])
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon)
+        let pokemonRecord = getPokemon(pokemon.url)
+        return pokemonRecord
+
+      })
+    )
+    setPokemonData(_pokemonData)
+  }
+
+  console.log(pokemonData)
+  
   return (
     <div className="App">
       {loading ? (
