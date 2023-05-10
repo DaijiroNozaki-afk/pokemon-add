811025e19cabe9be1c56e26fb3216cde0337e5af
dnozaki <dnozaki88888888@gmail.com>
2023-05-10 16:09:15 +0900

    get pokemon data 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon/"
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL)
+      console.log(res)
+
+    } 
+    fetchPokemonData()
+  }, [])
   return (
-    <div className="App">Hello!
-    </div>
+    <div className="App"></div>
   );
 }
 
